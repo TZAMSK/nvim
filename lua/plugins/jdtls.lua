89867a553246226c8ea9eaf81ea2a5594d236553
TZAMSK <alikhankhakimov@outlook.com>
2026-02-28 00:20:03 -0500

    search addition

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -1,5 +1,32 @@
--- NOTE: For Java
+-- NOTE: For Java with JavaFX support
 return {
   "mfussenegger/nvim-jdtls",
   ft = "java",
+  config = function()
+    -- Keymaps for JavaFX development
+    vim.keymap.set('n', '<leader>jfc', 
+      ':!javac --module-path /usr/lib/jvm/java-24.0.2-openjfx/lib --add-modules javafx.controls,javafx.fxml -d build %<CR>', 
+      { desc = "Compile current Java file with JavaFX" })
+    
+    vim.keymap.set('n', '<leader>jfr', 
+      ':!java --module-path /usr/lib/jvm/java-24.0.2-openjfx/lib --add-modules javafx.controls,javafx.fxml -cp build src.Main<CR>', 
+      { desc = "Run JavaFX app" })
+    
+    vim.keymap.set('n', '<leader>jfa', 
+      ':!javac --module-path /usr/lib/jvm/java-24.0.2-openjfx/lib --add-modules javafx.controls,javafx.fxml -d build src/*.java && java --module-path /usr/lib/jvm/java-24.0.2-openjfx/lib --add-modules javafx.controls,javafx.fxml -cp build src.Main<CR>', 
+      { desc = "Compile & Run all JavaFX" })
+    
+    -- JavaFX debug configuration for nvim-dap (if installed)
+    vim.keymap.set('n', '<leader>jfd', 
+      ':lua require("dap").run({type = "java", request = "launch", name = "Debug JavaFX", vmArgs = "--module-path /usr/lib/jvm/java-24.0.2-openjfx/lib --add-modules javafx.controls,javafx.fxml", mainClass = "src.Main", projectPaths = { vim.fn.getcwd() }})<CR>', 
+      { desc = "Debug JavaFX app" })
+    
+    -- Create build directory automatically
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.java",
+      callback = function()
+        vim.fn.system("mkdir -p build")
+      end
+    })
+  end
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,8 @@ return {
     lazy = false,
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp"  -- Make sure this is installed for capabilities
+      "hrsh7th/cmp-nvim-lsp",  -- Make sure this is installed for capabilities
+      "mfussenegger/nvim-jdtls"  -- Add jdtls as dependency here
     },
     config = function()
       vim.lsp.set_log_level("debug")  -- Temporarily enable for debugging
@@ -28,11 +29,18 @@ return {
       local lspconfig = require("lspconfig")
 
       local on_attach = function(client, bufnr)
+        -- Java-specific keymaps
+        if client.name == 'jdtls' then
+          vim.keymap.set('n', '<leader>jo', vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+          vim.keymap.set('n', '<leader>jr', vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
+          vim.keymap.set('n', '<leader>ji', vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
+        end
       end
 
       local servers = {
         "html", "lua_ls", "cssls", "ts_ls", "angularls", 
-        "rust_analyzer", "sqlls", "jdtls"
+        "rust_analyzer", "sqlls"
+        -- Removed "jdtls" from here since we configure it separately below
       }
 
       for _, server in ipairs(servers) do
@@ -42,17 +50,86 @@ return {
         })
       end
 
+      -- JavaFX specific configuration for jdtls
+      local javafx_path = "/usr/lib/jvm/java-24.0.2-openjfx/lib"
+      local javafx_modules = "javafx.controls,javafx.fxml"
+      
       lspconfig.jdtls.setup({
         cmd = {'jdtls'},
         capabilities = capabilities,
-        on_attach = on_attach
+        on_attach = on_attach,
+        root_dir = lspconfig.util.root_pattern(
+          '.git', 'gradlew', 'mvnw', 'pom.xml', 
+          'build.gradle', 'build.gradle.kts', '.project'
+        ),
+        settings = {
+          java = {
+            configuration = {
+              runtimes = {
+                {
+                  name = "JavaSE-24",
+                  path = "/usr/lib/jvm/java-25-openjdk",
+                }
+              }
+            },
+            project = {
+              referencedLibraries = {
+                javafx_path .. "/*.jar",
+                javafx_path .. "/javafx.base.jar",
+                javafx_path .. "/javafx.controls.jar",
+                javafx_path .. "/javafx.fxml.jar",
+                javafx_path .. "/javafx.graphics.jar",
+                javafx_path .. "/javafx.media.jar",
+                javafx_path .. "/javafx.swing.jar",
+                javafx_path .. "/javafx.web.jar"
+              }
+            },
+            completion = {
+              favoriteStaticMembers = {
+                "org.junit.Assert.*",
+                "org.junit.Assume.*",
+                "org.junit.jupiter.api.Assertions.*",
+                "org.junit.jupiter.api.Assumptions.*",
+                "org.junit.jupiter.api.DynamicContainer.*",
+                "org.junit.jupiter.api.DynamicTest.*",
+                "org.mockito.Mockito.*",
+                "org.mockito.ArgumentMatchers.*",
+                "org.mockito.Answers.*",
+                "java.util.Objects.requireNonNull",
+                "java.util.Objects.requireNonNullElse",
+                "org.hamcrest.MatcherAssert.assertThat",
+                "org.hamcrest.Matchers.*"
+              }
+            }
+          }
+        },
+        init_options = {
+          bundles = {},
+          extendedClientCapabilities = {
+            progressReportProvider = true,
+            classFileContentsSupport = true,
+            generateToStringPromptSupport = true,
+            hashCodeEqualsPromptSupport = true,
+            advancedExtractRefactoringSupport = true,
+            advancedOrganizeImportsSupport = true,
+            generateConstructorsPromptSupport = true,
+            generateDelegateMethodsPromptSupport = true,
+            moveRefactoringSupport = true,
+            overrideMethodsPromptSupport = true,
+            inferSelectionSupport = { "extractMethod", "extractVariable", "extractConstant" },
+          }
+        }
       })
-
-      --lspconfig.kotlin_language_server.setup({
-      --  cmd = { "C:/Program Files/kotlin-language-server-1.3.12/server/build/install/server/bin/kotlin-language-server" },
-      --  capabilities = capabilities,
-      --  on_attach = on_attach
-      --})
+      
+      -- Set JavaFX specific environment for Java files
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "java",
+        callback = function()
+          -- Set path to JavaFX modules for this buffer
+          vim.b.javafx_path = javafx_path
+          vim.b.javafx_modules = javafx_modules
+        end
+      })
     end,
   },
 }
