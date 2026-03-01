4c4d526d3fc3e9094fea45dc62baff96064dfbab
TZAMSK <alikhankhakimov@outlook.com>
2026-03-01 18:59:31 -0500

    Mason lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,135 +1,92 @@
 return {
+  -- Mason core package manager
   {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    dependencies = { "williamboman/mason.nvim" },
+    "mason-org/mason.nvim",
     opts = {
-      auto_install = true,
+      ui = { border = "rounded" },
     },
   },
+
+  -- Mason LSP config integration
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "lua_ls", "ts_ls", "cssls", "html", "angularls", "rust_analyzer", "sqlls" },
+      automatic_installation = true,
+    },
+  },
+
+  -- LSP server setup
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",  -- Make sure this is installed for capabilities
-      "mfussenegger/nvim-jdtls"  -- Add jdtls as dependency here
-    },
+    dependencies = { "mason-org/mason-lspconfig.nvim" },
     config = function()
-      vim.lsp.set_log_level("debug")  -- Temporarily enable for debugging
-
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
 
-      local on_attach = function(client, bufnr)
-        -- Java-specific keymaps
-        if client.name == 'jdtls' then
-          vim.keymap.set('n', '<leader>jo', vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
-          vim.keymap.set('n', '<leader>jr', vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename" })
-          vim.keymap.set('n', '<leader>ji', vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
-        end
-      end
-
-      local servers = {
-        "html", "lua_ls", "cssls", "ts_ls", "angularls", 
-        "rust_analyzer", "sqlls"
-        -- Removed "jdtls" from here since we configure it separately below
-      }
-
-      for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach
-        })
-      end
-
-      -- JavaFX specific configuration for jdtls
-      local javafx_path = "/usr/lib/jvm/java-24.0.2-openjfx/lib"
-      local javafx_modules = "javafx.controls,javafx.fxml"
-      
-      lspconfig.jdtls.setup({
-        cmd = {'jdtls'},
-        capabilities = capabilities,
-        on_attach = on_attach,
-        root_dir = lspconfig.util.root_pattern(
-          '.git', 'gradlew', 'mvnw', 'pom.xml', 
-          'build.gradle', 'build.gradle.kts', '.project'
-        ),
+      -- Lua LSP
+      lspconfig.lua_ls.setup({
+        single_file_support = true,
         settings = {
-          java = {
-            configuration = {
-              runtimes = {
-                {
-                  name = "JavaSE-24",
-                  path = "/usr/lib/jvm/java-25-openjdk",
-                }
-              }
+          Lua = {
+            workspace = { checkThirdParty = false },
+            completion = { workspaceWord = true, callSnippet = "Both" },
+            hint = {
+              enable = true,
+              setType = false,
+              paramType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
             },
-            project = {
-              referencedLibraries = {
-                javafx_path .. "/*.jar",
-                javafx_path .. "/javafx.base.jar",
-                javafx_path .. "/javafx.controls.jar",
-                javafx_path .. "/javafx.fxml.jar",
-                javafx_path .. "/javafx.graphics.jar",
-                javafx_path .. "/javafx.media.jar",
-                javafx_path .. "/javafx.swing.jar",
-                javafx_path .. "/javafx.web.jar"
-              }
+            diagnostics = {
+              disable = { "incomplete-signature-doc", "trailing-space" },
+              unusedLocalExclude = { "_*" },
             },
-            completion = {
-              favoriteStaticMembers = {
-                "org.junit.Assert.*",
-                "org.junit.Assume.*",
-                "org.junit.jupiter.api.Assertions.*",
-                "org.junit.jupiter.api.Assumptions.*",
-                "org.junit.jupiter.api.DynamicContainer.*",
-                "org.junit.jupiter.api.DynamicTest.*",
-                "org.mockito.Mockito.*",
-                "org.mockito.ArgumentMatchers.*",
-                "org.mockito.Answers.*",
-                "java.util.Objects.requireNonNull",
-                "java.util.Objects.requireNonNullElse",
-                "org.hamcrest.MatcherAssert.assertThat",
-                "org.hamcrest.Matchers.*"
-              }
-            }
-          }
+            format = { enable = false },
+          },
         },
-        init_options = {
-          bundles = {},
-          extendedClientCapabilities = {
-            progressReportProvider = true,
-            classFileContentsSupport = true,
-            generateToStringPromptSupport = true,
-            hashCodeEqualsPromptSupport = true,
-            advancedExtractRefactoringSupport = true,
-            advancedOrganizeImportsSupport = true,
-            generateConstructorsPromptSupport = true,
-            generateDelegateMethodsPromptSupport = true,
-            moveRefactoringSupport = true,
-            overrideMethodsPromptSupport = true,
-            inferSelectionSupport = { "extractMethod", "extractVariable", "extractConstant" },
-          }
-        }
       })
-      
-      -- Set JavaFX specific environment for Java files
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "java",
-        callback = function()
-          -- Set path to JavaFX modules for this buffer
-          vim.b.javafx_path = javafx_path
-          vim.b.javafx_modules = javafx_modules
+
+      -- TypeScript / JavaScript LSP
+      lspconfig.ts_ls.setup({
+        root_dir = lspconfig.util.root_pattern(".git"),
+        single_file_support = false,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "literal",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      })
+
+      -- Other LSP servers
+      local servers = { "cssls", "html", "angularls", "rust_analyzer", "sqlls" }
+      for _, server in ipairs(servers) do
+        if lspconfig[server] then
+          lspconfig[server].setup({})
+        else
+          vim.notify("LSP server not installed: " .. server, vim.log.levels.WARN)
         end
-      })
+      end
     end,
   },
 }
